77f567dfb74d25284464f363b2b32d4dbd10f6b2
xbiwas <biwas2059@gmail.com>
2024-05-30 12:02:11 +0545

    about

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Home />
+      <About />
     </div>
   );
 };
