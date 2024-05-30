7c09b49a8851a5bb405028d6f3ab8aab32ad6ce7
xbiwas <biwas2059@gmail.com>
2024-05-30 11:14:07 +0545

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 };
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://github.com/xbiwas"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+
+      <a
+        href="https://github.com/xbiwas"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/biwas-bhandari/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
